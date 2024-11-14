e9844b6f74f342e053300c46c798aacb207db2fe
mohizzatulhaqi <161985566+mohizzatulhaqi@users.noreply.github.com>
2024-11-14 13:06:01 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,14 +1,1 @@
-<?php
 
-$server = "localhost:3308";
-$user = "root";
-$password = "";
-$nama_database = "pendaftaran_siswarpl";
-
-$db = mysqli_connect($server, $user, $password, $nama_database);
-
-if( !$db ){
-    die("Gagal terhubung dengan database: " . mysqli_connect_error());
-}
-
-?>
